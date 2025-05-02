4665af4f766b98fbcff4354d67489e66e885b827
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-03 00:17:12 +0530

    Cpp: Add test cases1.11

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,15 +1,22 @@
+#include <iostream>
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
-    public:
-        ListNode* reverseList(ListNode* head) {
-            ListNode *nextNode, *prevNode = NULL;
-            while (head) {
-                nextNode = head->next;
-                head->next = prevNode;
-                prevNode = head;
-                head = nextNode;
-            }
-            return prevNode;
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *nextNode, *prevNode = NULL;
+        while (head) {
+            nextNode = head->next;
+            head->next = prevNode;
+            prevNode = head;
+            head = nextNode;
         }
-        // Function to reverse a singly linked list........................///
-    };
-    
+        return prevNode;
+    }
+};
