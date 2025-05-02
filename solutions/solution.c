bb979f9f81272f531c43556c9bf6afacc64f5303
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-02 23:57:11 +0530

    C: Add test cases3.122

--- solutions/solution.c
+++ solutions/solution.c
@@ -10,16 +10,5 @@ struct ListNode {
 
 // Function to reverse a singly linked list......................../
 struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode* prev = NULL;
-    struct ListNode* current = head;
-    struct ListNode* next = NULL;
-
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    return prev;
+   // write your code here 
 }
