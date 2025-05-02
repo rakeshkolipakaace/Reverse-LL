65b696e73683ec932515688e2ea63f217867ae5e
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-03 01:01:22 +0530

    java: Add test cases

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -20,16 +20,7 @@ public class Solution {
 
     // Function to reverse the linked list
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;  // Previous node (initially null)
-        ListNode curr = head;  // Current node (starts at head)
-        
-        while (curr != null) {
-            ListNode nextTemp = curr.next;  // Store the next node
-            curr.next = prev;  // Reverse the current node's pointer
-            prev = curr;  // Move prev and curr one step forward
-            curr = nextTemp;
-        }
-        
-        return prev;  // Prev will be the new head of the reversed list
+
+       // write your code here
     }
 }
