dd338d7e0e0ca0448b0e041d1fe5bab8ffed91fe
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 12:52:28 +0530

    base cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -10,14 +10,6 @@ public:
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-         ListNode *nextNode, *prevNode = NULL;
-        while (head) {
-            nextNode = head->next;
-            head->next = prevNode;
-            prevNode = head;
-            head = nextNode;
-        }
-        return prevNode;
   
        // Write your code here
     }
