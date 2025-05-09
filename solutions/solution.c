f7fdde458215eaa48dc1b3c08a0272c967a15915
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 12:58:25 +0530

    c solution

--- solutions/solution.c
+++ solutions/solution.c
@@ -10,6 +10,19 @@ struct ListNode {
 
 
 struct ListNode* reverseList(struct ListNode* head) {
+
+     struct ListNode* prev = NULL;
+    struct ListNode* current = head;
+    struct ListNode* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;   
+        current->next = prev;   
+        prev = current;         
+        current = next;
+    }
+
+    return prev; 
     
-   // write your code here 
+
 }
